ca9268db94f92421bea5a2f78f04c1b81fda2dd6
bernadiussuprihatintejosriyanto <145355661+bernadiussuprihatintejosriyanto@users.noreply.github.com>
2023-10-02 19:39:39 +0700

    Delete latihan1.php

--- latihan1.php
+++
@@ -1,9 +0,0 @@
-<?php
-class contoh1 extends CI_Controller
-{
-    public function index()
-    {
-        echo "<h1>Perkenalkan nama saya</h1>";
-        echo"asdddddddddddddddddddddddddd";
-    }
-}
